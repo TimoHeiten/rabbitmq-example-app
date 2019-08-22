77ed7a6db5c2182edd2d7bf7c3c0c72297e092ab
timo.heiten <heitech.heiten@gmail.com>
2019-08-22 08:46:44 +0200

    latest changes to RestApi

--- csharp/Udemy.Merchant.Rest/Startup.cs
+++ csharp/Udemy.Merchant.Rest/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EasyNetQ;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,7 +27,7 @@ namespace Udemy.Merchant.Rest
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            
+            services.AddSingleton(typeof(IBus), p => Udemy.Merchant.Bus.Factory.PrepareBus("dotnet_user", "dotnet"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
